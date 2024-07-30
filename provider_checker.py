dabb3f29ce8384593a319f258d192a3a76df6a17
Szuriel <ej.wesson@gmail.com>
2024-07-30 13:40:33 -0500

    Provider Checker first upload
    Checking one provider

---
+++ provider_checker.py
@@ -0,0 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
+
+# List of URLs to check
+websites = [
+    "https://node1.jackalstorageprovider40.com/version",
+#   "add providers here",
+    
+]
+
+# searching
+search_text = "1.0.0"
+
+def check_website(url, search_text):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  
+        content = response.text
+        
+        
+        soup = BeautifulSoup(content, 'html.parser')
+        
+        
+        if search_text in soup.get_text():
+            return True
+        else:
+            return False
+    except requests.RequestException as e:
+        print(f"Error fetching {url}: {e}")
+        return False
+
+# Loopty loop
+for site in websites:
+    if check_website(site, search_text):
+        print(f"Site {site} can receive grants.")
+    else:
+        print(f"Site {site} cannot receive grants.")
